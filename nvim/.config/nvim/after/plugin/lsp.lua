41c8cb1ba222988157ca011c6ad324b278b6b725
willwatson27 <willwatson27@gmail.com>
2024-11-23 03:05:48 -0800

    sync

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -40,7 +40,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'lua_ls', 'ts_ls', 'bashls'},
+  ensure_installed = {'lua_ls', 'ts_ls', 'jsonls', 'bashls'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
